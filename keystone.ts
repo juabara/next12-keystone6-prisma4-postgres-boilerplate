16066057f38cd3192743f69f35c92bf2d7c2f638
Juarez Mota <juabara@gmail.com>
2022-08-08 20:22:21 +0100

    feat: add instrumentation

--- keystone.ts
+++ keystone.ts
@@ -1,10 +1,32 @@
-import { config } from "@keystone-6/core";
+import { config, list } from '@keystone-6/core';
+
+import { Lists } from '.keystone/types';
+import { text } from '@keystone-6/core/fields';
+
+const deleteMeAfterFirstRealSchema: Lists.DeleteMeAfterFirstRealSchema = list({
+  fields: {
+    title: text({ validation: { isRequired: true } }),
+    slug: text({ isIndexed: 'unique', isFilterable: true }),
+    content: text(),
+  },
+});
 
 export default config({
-  db: { provider: "postgresql", url: "file:./app.db" },
+  db: {
+    provider: 'postgresql',
+    url: String(process.env.DATABASE_URL),
+    onConnect: async (context) => {
+      /* ... */
+    },
+    // Optional advanced configuration
+    enableLogging: true,
+    useMigrations: true,
+    idField: { kind: 'uuid' },
+    shadowDatabaseUrl: String(process.env.SHADOW_DATABASE_URL),
+  },
   experimental: {
     generateNextGraphqlAPI: true,
     generateNodeAPI: true,
   },
-  lists: {},
+  lists: { DeleteMeAfterFirstRealSchema: deleteMeAfterFirstRealSchema },
 });
